37489aa3e313cbf5f9c6ef1568471effbe53ee70
ffabss <fabian.buchenberger@gmail.com>
2019-10-11 12:02:58 +0200

    added Class Clazz

--- src/sample/Model.java
+++ src/sample/Model.java
@@ -1,9 +1,62 @@
 package sample;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
     private Controller controller;
 
-    public Model(Controller controller){
+    public Model(Controller controller) {
         this.controller = controller;
+    }
+
+    public List<Clazz> parse(File file) {
+        List<Clazz> clazzes = new ArrayList<>();
+        try {
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("graph");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("data: " + eElement.getAttribute("data"));
+                    System.out.println("node : " + eElement.getElementsByTagName("node").item(0).getTextContent());
+                    System.out.println("Last Name : " + eElement.getElementsByTagName("node").item(1).getTextContent());
+                    System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+                    System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return clazzes;
     }
 }
