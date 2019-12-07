91ed2022b1aa968975a39bf48deb135046852eb0
Schoergi1 <alex.schoergi@gmail.com>
2019-12-07 08:05:02 +0100

    test begin

--- src/Tests/ModelTest.java
+++ src/Tests/ModelTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import sample.Clazz;
 import sample.Controller;
 import sample.Model;
+import sample.Parser;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -14,11 +15,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class ModelTest {
 
     Model model = new Model(new Controller());
+    Parser parser = new Parser(model);
     @Test
     void parse() {
         List<Clazz> clazzes = new ArrayList<>();
-        clazzes = model.parse(new File("employee.graphml"));
+        clazzes = parser.parse(new File("employee.graphml"));
         System.out.println(clazzes);
         assertEquals(clazzes.get(0).getName(), "Employee");
+    }
+
+    @Test
+    void parse2() {
+        List<Clazz> clazzes = new ArrayList<>();
+        clazzes = parser.parse(new File("main.graphml"));
+        System.out.println(clazzes);
+        if(clazzes.get(0).getName() == "Main" && clazzes.get(0).getVariables().get(0) == "final static - SCANNER : Scanner"
+        && clazzes.get(0).getVariables().get(1) == "final static - STORE : Store"
+        && clazzes.get(0).getVariables().get(2) == "static - user : User"
+                && clazzes.get(0).getMetohds().get(0) == "static + main(args : String[]) : void"
+        ){
+            assertTrue(true);
+        }
+
     }
 }
