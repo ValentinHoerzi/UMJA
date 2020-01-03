11a0484c084e55d6ea628b14b1223f05392db018
Schoergi1 <alex.schoergi@gmail.com>
2020-01-03 13:37:53 +0100

    test update

--- src/Tests/ModelTest.java
+++ src/Tests/ModelTest.java
@@ -82,4 +82,72 @@ class ModelTest {
 
         assertTrue(right);
     }
+
+
+    @Test
+    void parseFullUml() {
+        List<Clazz> clazzes = new ArrayList<>();
+        clazzes = parser.parse(new File("uml.graphml"));
+        System.out.println(clazzes);
+        boolean right = false;
+
+        if(clazzes.get(0).getName().equals("Main")
+                && clazzes.get(0).getVariables().get(0).equals("final static - SCANNER : Scanner")
+                && clazzes.get(0).getVariables().get(1).equals("final static - STORE : Store")
+                && clazzes.get(0).getVariables().get(2).equals("static - user : User")
+                && clazzes.get(0).getMetohds().get(0).equals("static + main(args : String[]) : void"))
+        {
+            if (clazzes.get(1).getName().equals("GameType")
+                    && clazzes.get(1).getVariables().get(0).equals("ACTTION, ROLEPLAY, \nSTRATEGY, SPORT")
+                    && clazzes.get(1).getMetohds().equals(null)){
+                if (clazzes.get(2).getName().equals("Game")
+                        && clazzes.get(2).getVariables().get(0).equals("- name : String")
+                        && clazzes.get(2).getVariables().get(1).equals("- price : String")
+                        && clazzes.get(2).getVariables().get(2).equals("- published : String")
+                        && clazzes.get(2).getVariables().get(3).equals("- type : GameType")
+                        && clazzes.get(2).getMetohds().get(0).equals("+ Customer(name : String)")
+                        && clazzes.get(2).getMetohds().get(1).equals("+ getLibrary() : Game[]")
+                        && clazzes.get(2).getMetohds().get(2).equals("+ addToLibrary(game : Game) : void")
+                        && clazzes.get(2).getMetohds().get(3).equals("+ toString() : String")
+                        && clazzes.get(2).getImports().get(0).equals("net.htlgrieskirchen.pos2.plf.retrosteam.user.User")
+                        && clazzes.get(2).getImplementations().get(0).equals("User")){
+                    if (){
+                        if(clazzes.get(4).getName().equals("Customer")
+                                && clazzes.get(4).getVariables().get(0).equals("- name : String")
+                                && clazzes.get(4).getVariables().get(1).equals("- library : Game[]")
+                                && clazzes.get(4).getVariables().get(2).equals("- wishlist : Game[]")
+                                && clazzes.get(4).getMetohds().get(0).equals("+ Customer(name : String)")
+                                && clazzes.get(4).getMetohds().get(1).equals("+ getLibrary() : Game[]")
+                                && clazzes.get(4).getMetohds().get(2).equals("+ addToLibrary(game : Game) : void")
+                                && clazzes.get(4).getMetohds().get(3).equals("+ toString() : String")
+                                && clazzes.get(4).getImports().get(0).equals("net.htlgrieskirchen.pos2.plf.retrosteam.user.User")
+                                && clazzes.get(4).getImplementations().get(0).equals("User")
+                        ){
+                            if(clazzes.get(5).getName().equals("User")
+                                    && clazzes.get(5).getVariables().get(0).equals("final static + CAPACITY = 2 : int")
+                                    && clazzes.get(5).getMetohds().get(0).equals("+ getWishlist() : Game[]")
+                                    && clazzes.get(5).getMetohds().get(1).equals("+ addToWishlist(game : Game) : void")
+                            ){
+                                if(clazzes.get(6).getName().equals("Visitor")
+                                        && clazzes.get(6).getVariables().get(0).equals("- wishlist : Game[]")
+                                        && clazzes.get(6).getMetohds().get(0).equals("+ Visitor()")
+                                        && clazzes.get(6).getMetohds().get(1).equals("+ toString() : String")
+                                        && clazzes.get(6).getImports().get(0).equals("net.htlgrieskirchen.pos2.plf.retrosteam.user.User")
+                                        && clazzes.get(6).getImplementations().get(0).equals("User")
+                                ){
+                                    right = true;
+                                }
+                            }
+                    }
+                }
+            }
+
+            }
+
+            assertTrue(right);
+        }
+        }
+
+
+
 }
