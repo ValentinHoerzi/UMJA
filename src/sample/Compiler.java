4311c9ee5518a9ba943d06cbef949c2ca74ae974
ascheuringer <47625910+Pauli9966@users.noreply.github.com>
2020-01-03 14:21:34 +0100

    es schreibt den dreck endlich richtig

--- src/sample/Compiler.java
+++ src/sample/Compiler.java
@@ -2,9 +2,13 @@ package sample;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -12,9 +16,12 @@ import java.util.regex.Pattern;
 
 public class Compiler {
 
-    private List<Clazz> allClazzes;
+    //private List<Clazz> allClazzes;
+    private String path;
 
     public void compile(List<Clazz> allClazzes, String path) {
+        this.path = path;
+
         List<String> methods = new ArrayList<>();
         List<String> variables = new ArrayList<>();
         for (Clazz clazz : allClazzes) {
@@ -45,6 +52,7 @@ public class Compiler {
                 fileText.add("import " + item + ";");
             }
         }
+        fileText.add("import sun.reflect.generics.reflectiveObjects.NotImplementedException;");
 
         //class - implementations
         if (clazz.getImplementations() == null || clazz.getImplementations().isEmpty()) {
@@ -55,7 +63,7 @@ public class Compiler {
             //go through all implementations
             for (int i = 0; i < clazz.getImplementations().size(); i++) {
                 // if it is the last implementation no komma but {
-                if (i == clazz.getImplementations().size()) {
+                if (i == clazz.getImplementations().size() - 1) {
                     classLine += " " + clazz.getImplementations().get(i) + "{ ";
 
                 } else {
@@ -69,6 +77,138 @@ public class Compiler {
         //final static - SCANNER : Scanner
         // variables
         for (String item : clazz.getVariables()) {
+            if (createVariable(item) != null)
+                fileText.add(createVariable(item));
+        }
+
+        // methods
+
+        for (String item : clazz.getMetohds()) {
+            if (createMethod(item) != null)
+                fileText.add(createMethod(item) + "){throw new NotImplementedException();}");
+        }
+
+        //close class
+        fileText.add("}");
+
+        try {
+            writeFile("/Clazz.java", fileText);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private String createMethod(String item) {
+        try {
+            String method = "";
+            String[] parts = item.split(" ");
+            List<String> list = new LinkedList<String>(Arrays.asList(parts));
+
+            //region public private static
+
+            if (item.contains("+")) {
+                method += "public ";
+                list.remove("+");
+            }
+            if (item.contains("-")) {
+                method += "private ";
+                list.remove("-");
+            }
+            if (item.contains("static")) {
+                method += "static ";
+                list.remove("static");
+            }
+//endregion
+
+            // + Class (user:User, List():list)
+            // + Class (user:User, List():list) : String
+
+            if (!parts[parts.length - 1].contains(")")) { // it is no konstruktor
+                //region method
+                // fügt rückgabetyp der methode dazu zb void, int, String
+                method += parts[parts.length - 1];
+                list.remove(parts[parts.length - 1]);
+
+                // name der methode mit (
+                method += " " + list.get(0).split("\\(")[0] + "(";
+                list.set(0, list.get(0).split("\\(")[1]);
+
+
+                //removing all doublepoints from list
+                boolean cont = true;
+                while (cont) {
+                    cont = list.remove(":");
+                }
+
+                //remove last klammer
+                list.set(list.size() - 1,
+                        list.get(list.size() - 1)
+                                .substring(0, list.get(list.size() - 1)
+                                        .length() - 1));
+
+                if (list.size() > 2) {
+                    for (int i = 1; i < list.size(); i += 2) {
+                        method += list.get(i).split(",")[0] + " ";
+                        method += list.get(i - 1) + ", ";
+                        //user
+                        //User,
+                    }
+                } else if (list.size() > 1) {
+                    method += list.get(1).split(",")[0] + " ";
+                    method += list.get(0) + ", ";
+                }
+
+                //remove last "," and add to filetext
+                if (method.contains(","))
+                    method = (method.substring(0, method.length() - 2));
+                //endregion
+            } else { // its a konstruktor
+                //region create konstruktor
+                // name der methode mit (
+                method += " " + list.get(0).split("\\(")[0] + "(";
+                list.set(0, list.get(0).split("\\(")[1]);
+
+                if (list.size() > 1) {
+                    //removing all doublepoints from list
+                    boolean cont = true;
+                    while (cont) {
+                        cont = list.remove(":");
+                    }
+
+                    //remove last klammer
+                    list.set(list.size() - 1,
+                            list.get(list.size() - 1)
+                                    .substring(0, list.get(list.size() - 1)
+                                            .length() - 1));
+
+                    if (list.size() > 2) {
+                        for (int i = 1; i < list.size(); i += 2) {
+                            method += list.get(i).split(",")[0] + " ";
+                            method += list.get(i - 1) + ", ";
+                            //user
+                            //User,
+                        }
+                    } else if (list.size() > 1) {
+                        method += list.get(1).split(",")[0] + " ";
+                        method += list.get(0) + ", ";
+                    }
+
+                    //remove last "," and add to filetext
+                    if (method.contains(","))
+                        method = (method.substring(0, method.length() - 2));
+                }
+
+                //endregion
+            }
+            return method;
+        } catch (Exception ex) {
+            System.out.println("method ->  " + item);
+            return null;
+        }
+    }
+
+    private String createVariable(String item) {
+        try {
             String variable = "";
 
             // add public|static final static
@@ -84,65 +224,94 @@ public class Compiler {
             if (item.contains("static")) {
                 variable += "static ";
             }
-
-            // add name and type of variable
             String[] parts = item.split(" ");
-            variable += parts[parts.length - 1] + " " + parts[parts.length - 3] + ";";
-
-            fileText.add(variable);
+            if (!item.contains("=")) {
+                // add name and type of variable
+                variable += parts[parts.length - 1] + " " + parts[parts.length - 3] + ";";
+            } else {
+                variable += parts[parts.length - 1] + " ";
+                String[] foo = item.split("=")[0].split(" ");
+                variable += foo[foo.length - 1] + " ";
+                String[] baa = item.split("=")[1].split(":");
+                variable += "=" + baa[0] + ";";
+            }
+            return variable;
+        } catch (Exception ex) {
+            System.out.println("variable ->  " + item);
+            return null;
         }
-
-        // methods
-        // static + main(args:String[], name:Type) : void
-
-        for (String item : clazz.getMetohds()) {
-            String method = "";
-            if (item.contains("+")) {
-                method += "public ";
-            }
-            if (item.contains("-")) {
-                method += "private ";
-            }
-            if (item.contains("static")) {
-                method += "static ";
-            }
-
-            String[] parts = item.split(" ");
-            method += parts[parts.length - 1];
-
-
-        }
-
-        //close class
-        fileText.add("}");
     }
 
     private void createEnum(Clazz clazz) {
+        //list with text which gets written in file
+        List<String> fileText = new LinkedList<>();
+        //package
+        fileText.add("package " + clazz.getNameSpace() + ";");
 
+        fileText.add("public enum " + clazz.getName() + "{");
+
+        for (String item : clazz.getVariables()) {
+            item = item.replaceAll("\n", "");
+            for (String value : item.split(",")) {
+                fileText.add(value.toUpperCase() + ",");
+            }
+        }
+        fileText.set(fileText.size() - 1, fileText.get(fileText.size() - 1).replace(",", ""));
+        fileText.add(";}");
+        try {
+            writeFile("/enum.java", fileText);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     private void createInterface(Clazz clazz) {
+        //list with text which gets written in file
+        List<String> fileText = new LinkedList<>();
+        //package
+        fileText.add("package " + clazz.getNameSpace() + ";");
 
+        fileText.add("interface " + clazz.getName() + "{");
+
+
+        //variable: final static + CAPACITY = 2:int
+        for (String item : clazz.getVariables()) {
+            if (createVariable(item) != null)
+                fileText.add(createVariable(item));
+        }
+
+        //method: +getWishlist():Game[]
+        // + addToWishlist(game : Game):void
+
+        for (String item : clazz.getMetohds()) {
+            if (createMethod(item) != null)
+                fileText.add(createMethod(item) + ");");
+        }
+
+        //write text to file
+        fileText.add("}");
+        try {
+            writeFile("/interface.java", fileText);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private void writeFile(String path, List<String> fileString) {
-        try (FileWriter fileWriter = new FileWriter(path)) {
-
+    private void writeFile(String pathCustom, List<String> fileString) throws IOException {
+        String totalpath = path + pathCustom;
+        Files.createDirectories(Paths.get(path));
+        try (
+                FileWriter writer = new FileWriter(new File(totalpath));
+                BufferedWriter fileWriter = new BufferedWriter(writer)
+        ) {
             for (String line : fileString) {
-                fileWriter.write(line);
+                fileWriter.write(line + "\n");
             }
+            fileWriter.flush();
 
         } catch
         (IOException ex) {
             ex.printStackTrace();
         }
-    }
-
-    private List<String> createMethodFromString(Clazz clazz) {
-        return null;
-    }
-
-    private List<String> createVariableFromString(Clazz clazz) {
-        return null;
     }
 }
